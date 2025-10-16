bbae9ac9ada9f11de1cea4bd3677be999fcf931e
usrchnraj <rajesh@spinesurgeon.london>
2025-10-16 17:32:14 +0530

    Ignore eslint in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // other Next.js config options if you add later
+
+  eslint: {
+    // ✅ allows production builds to succeed even if ESLint errors exist
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
